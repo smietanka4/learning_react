84624a5fd7ea26476dc21921e8762470ed9e8384
smietanka4 <wloczewskikarol5@gmail.com>
2025-11-05 23:42:42 +0100

    zad 1 poczatek

---
+++ front-end-nauka/zad1/app.js
@@ -0,0 +1,5 @@
+const loading = document.getElementById("loading");
+const error = document.getElementById("error");
+const list = document.getElementById("pokemon-list");
+const details = document.getElementById("pokemon-details");
+const search = document.getElementById("search");
